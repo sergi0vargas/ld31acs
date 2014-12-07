7842af490c7324dc4f9098cf0c2f58961bf107f9
Sergio Vargas <sergiovargas9@gmail.com>
2014-12-07 02:17:20 -0300

    sprite del player final v1 OK Movimiento del player OK, falta calibrar velocidad - sincronizar animacion. animaciones OK agregado efecto de particulas en el player agregada textura de pruebas del suelo

--- Assets/WorkingFolder/Sergio/Scripts/PlayerController.cs
+++ Assets/WorkingFolder/Sergio/Scripts/PlayerController.cs
@@ -9,69 +9,104 @@ public class PlayerController : MonoBehaviour {
 
 
     private Vector2 input;
+    private Animator anim;
 
+    private bool rightWalk = true;
+    private bool upWalk = false;
+    private bool noMovHorizontal;
+    private bool noMovVertical;
 
 	// Use this for initialization
 	void Start () {
 
-        Debug.Log(Vector3.forward);
-
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         input = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed, Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
-        
-        //Moviemiento sin fisicas
-        //transform.Translate(new Vector3(input.x, input.y, 0));
+
+        Animaciones();
+        RotacionDelSprite();
 	}
 
     void FixedUpdate()
     {
         //Movimiento
         rigidbody2D.AddForce(input);
-
-        //ROTACION!!!
-        {
-            if (input.magnitude != 0)
-                if (input.y > 0)
-                {
-                    if (input.x > 0)
-                    {
-                        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 360), Time.deltaTime);
-                        transform.rotation = Quaternion.Euler(Vector3.forward * 1);
-
-                    } if (input.x < 0)
-                    {
-                        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 180), Time.deltaTime);
-                        transform.rotation = Quaternion.Euler(Vector3.forward * 180);
-                    }
-                    else
-                    {
-                        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 90), Time.deltaTime);
-                        transform.rotation = Quaternion.Euler(Vector3.forward * 90);
-                    }
-                }
-                else
-                {
-                    if (input.x > 0)
-                    {
-                        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 360), Time.deltaTime);
-                        transform.rotation = Quaternion.Euler(Vector3.forward * 0);
-
-                    } if (input.x < 0)
-                    {
-                        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 180), Time.deltaTime);
-                        transform.rotation = Quaternion.Euler(Vector3.forward * 180);
-                    }
-                    else
-                    {
-                        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.forward * 270), Time.deltaTime);
-                        transform.rotation = Quaternion.Euler(Vector3.forward * 270);
-                    }
-                }
-        }
-
     }
+
+    void Animaciones()
+    {
+        if (input.magnitude != 0)
+            anim.SetBool("Walking", true);
+        else
+            anim.SetBool("Walking", false);
+    }
+
+    void RotacionDelSprite()
+    {
+        //Consulta si se esta moviendo en una direccion
+        if (input.x > 0){
+            rightWalk = true;
+            noMovHorizontal = false;
+        }
+        else if (input.x < 0){
+            rightWalk = false;
+            noMovHorizontal = false;
+        }
+        else
+            noMovHorizontal = true;
+
+        if (input.y > 0)
+        {
+            upWalk = true;
+            noMovVertical = false;
+        }
+        else if (input.y < 0)
+        {
+            upWalk = false;
+            noMovVertical = false;
+        }
+        else
+            noMovVertical = true;
+
+
+        //combina los movimientos en diagonales
+        if (!noMovHorizontal && !noMovVertical)
+        {
+            if (rightWalk && upWalk)
+                transform.rotation = Quaternion.Euler(Vector3.forward * 45);
+            else if (rightWalk && !upWalk)
+                transform.rotation = Quaternion.Euler(Vector3.forward * 315);
+            else if (!rightWalk && upWalk)
+                transform.rotation = Quaternion.Euler(Vector3.forward * 135);
+            else if (!rightWalk && !upWalk)
+                transform.rotation = Quaternion.Euler(Vector3.forward * 225);
+        }
+        else
+        {
+            if (noMovHorizontal)
+            {
+                if(upWalk)
+                    transform.rotation = Quaternion.Euler(Vector3.forward * 90);
+                else
+
+                    transform.rotation = Quaternion.Euler(Vector3.forward * 270);
+            }
+            if (noMovVertical)
+            {
+                if (rightWalk)
+                    transform.rotation = Quaternion.Euler(Vector3.forward * 0);
+                else
+
+                    transform.rotation = Quaternion.Euler(Vector3.forward * 180);
+            }
+        }
+            
+            
+        
+    }
+    
 }

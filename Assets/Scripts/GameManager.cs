a563a3aa95355eecb02d0a8f2c308c195cd18bfc
Sergio Vargas <sergiovargas9@gmail.com>
2014-12-08 18:24:04 -0300

    LifeBar ok GameManager OK Key manager OK

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,10 @@ public class GameManager : MonoBehaviour {
     public static GameManager manager;
 
     public float playerLife = 100;
+    public float damage = 25;
     public Text txtFlotante;
     public Image imgTrofeo;
+    public Slider lifeSlider;
     public RectTransform posTxtFlotante;
     
     void Awake()
@@ -27,15 +29,34 @@ public class GameManager : MonoBehaviour {
     void Start()
     {
         txtFlotante = GameObject.FindGameObjectWithTag("FloatingText").GetComponent<Text>();
-        imgTrofeo = GameObject.FindGameObjectWithTag("Trofeo").GetComponent<Image>();
-        posTxtFlotante = txtFlotante.transform as RectTransform;
-        DesactivarTrofeo();
+        if(txtFlotante != null)
+            posTxtFlotante = txtFlotante.transform as RectTransform;
+
+        imgTrofeo = GameObject.FindGameObjectWithTag("Key").GetComponent<Image>();
+        if(imgTrofeo != null)
+            DesactivarTrofeo();
+
+        lifeSlider = GameObject.FindGameObjectWithTag("LifeSlider").GetComponent<Slider>();
+        if (lifeSlider != null)
+            lifeSlider.value = playerLife;
     }
+
+    //Apply Damage - If the player life is less than 0 return true
+    public bool ApplyDamage()
+    {
+        playerLife -= damage;
+        if (playerLife <= 0)
+            return true;
+        
+        return false;
+    }
+
 
     public void ActivarTrofeo()
     {
         imgTrofeo.color = new Color(255, 255, 255, 255);
     }
+
     public void DesactivarTrofeo()
     {
         imgTrofeo.color = new Color(0, 0, 0, 0);
